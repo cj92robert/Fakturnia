efeb2676f5b9d21a0709c458343c092112845cee
Robert <cj92robert@gmail.com>
2020-09-12 17:45:42 +0200

    Added error object. close #9

---
+++ src/main/java/com/example/demo/models/Error.java
@@ -0,0 +1,20 @@
+package com.example.demo.models;
+
+public class Error {
+    private String description;
+
+    public Error() {
+    }
+
+    public Error(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
